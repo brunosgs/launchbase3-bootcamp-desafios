a2ce8e2fac0981732d206f232a46cb60f876cdd9
Bruno Gomes <brunosoges@gmail.com>
2020-03-10 13:04:35 -0300

    Finished challenge 02-3

--- fase2/02/desafio-02-3/assets/js/script.js
+++ fase2/02/desafio-02-3/assets/js/script.js
@@ -1,11 +1,17 @@
 const modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
 const cards = document.querySelectorAll('.card');
 
 for (const card of cards) {
     card.addEventListener("click", function() {
         const videoId = card.getAttribute('id')
+
         modalOverlay.classList.add('active');
         modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${videoId}`;
+
+        if (modal.classList.contains('maximize')) {
+            modal.classList.remove('maximize')
+        }
     });
 }
 
@@ -14,4 +20,11 @@ document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.querySelector('iframe').src = "";
 });
 
-document.querySelector('.modal')
+
+document.querySelector('.btn-maximize').addEventListener("click", function() {
+    if (modal.classList.contains('maximize')) {
+        modal.classList.remove('maximize')
+    } else {
+        modal.classList.add('maximize')
+    }
+});
