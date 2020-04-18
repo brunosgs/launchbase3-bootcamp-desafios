c9140a0592fcd7b81da9515038104b3317f05be9
Bruno Gomes <brunosoges@gmail.com>
2020-04-18 20:04:52 -0300

    Finished challenge 03-3

---
+++ fase2/03/desafio-03-3/public/assets/js/script.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector('.modal');
+const cards = document.querySelectorAll('.card');
+
+for (const card of cards) {
+    card.addEventListener("click", function() {
+        const cardId = card.getAttribute('id')
+
+        window.location.href = `/courses/${cardId}`
+    });
+}
